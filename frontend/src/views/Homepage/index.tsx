219bca0d221f0ff1cf08122cde3db006855a1066
Timmy Huang <thuang@chanzuckerberg.com>
2021-02-23 16:09:05 -0800

    thuang-916-import-csv

--- frontend/src/views/Homepage/index.tsx
+++ frontend/src/views/Homepage/index.tsx
@@ -3,11 +3,38 @@ import React, { FC } from "react";
 import Collections from "src/components/Collections";
 
 const Homepage: FC = () => {
+  const handleCSV = async (event) => {
+    const timeStart = Date.now();
+
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsText(file);
+
+    const csv = await file.text();
+
+    const rows = csv.split(/\r\n|\n/);
+    const headers = rows[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+    const result = rows.map((row) => row.split(","));
+    const json = JSON.stringify({ result });
+
+    // DEBUG
+    // DEBUG
+    // DEBUG
+    console.log("--------file", file);
+    console.log("--------headers", headers);
+    console.log("--------result", result);
+    console.log("--------json", json);
+
+    window.alert(`DONE! It took ${Date.now() - timeStart} ms`);
+  };
+
   return (
     <>
       <Head>
         <title>cellxgene | Homepage</title>
       </Head>
+      <input type="file" accept="csv" onChange={handleCSV} />
       <Collections />
     </>
   );
